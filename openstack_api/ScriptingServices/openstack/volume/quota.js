1359ad2a082001766a66aa896fae34f85298f0e3
ThuF <jordan.p.pavlov@gmail.com>
2017-04-28 17:25:25 +0300

    Adds "Volume -> Quota" API

---
+++ openstack_api/ScriptingServices/openstack/volume/quota.js
@@ -0,0 +1,28 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+
+var serviceEndpoints = require('openstack/serviceEndpoints');
+var httpResponse = require('net/http/response');
+var httpClient = require('net/http/client');
+
+exports.updateQuota = function(auth, projectId, quota) {
+	var url = serviceEndpoints.getVolumeService() + '/v2/' + auth.entity.token.project.id + '/os-quota-sets/' + projectId;
+	
+	var response = httpClient.put(url, {
+		'headers': [{
+			'name': 'Content-Type',
+			'value': 'application/json'
+		}, {
+			'name': 'X-Auth-Token',
+			'value': auth.token
+		}],
+		'body': JSON.stringify(quota)
+	});
+
+	if (response.statusCode !== httpResponse.OK) {
+		console.error('Error occured during Network Quota update: [' + response.statusCode + '] ' + response.statusMessage);
+		console.error('Error response: ' + response.data);
+	}
+
+	return JSON.parse(response.data);
+};
