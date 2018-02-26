dce67bce927df083184213f1b3384031320f3834
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-02-26 10:05:54 +0200

    Update FloatingIps.js

--- openstack/compute/FloatingIps.js
+++ openstack/compute/FloatingIps.js
@@ -5,27 +5,27 @@ var method = FloatingIpsApi.prototype = Object.create(Api);
 method.constructor = FloatingIpsApi;
 
 function FloatingIpsApi(token) {
-    Api.constructor.apply(this, [token]);
+	Api.constructor.apply(this, [token]);
 }
 
 method.getMetadata = function() {
 	return {
-    	'host': ServiceRegistry.getComputeService(),
-    	'version': 'v2.1',
-    	'kind': 'os-floating-ips'
-    };
+		'host': ServiceRegistry.getComputeService(),
+		'version': 'v2.1',
+		'kind': 'os-floating-ips'
+	};
 };
 
 method.list = function(queryParameters) {
-    var response = Api.list.call(this, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.floating_ips;
+	var response = Api.list.call(this, queryParameters);
+	var entity = JSON.parse(response.text);
+	return entity.floating_ips;
 };
 
 method.get = function(id, queryParameters) {
-    var response = Api.get.call(this, id, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.floating_ip;
+	var response = Api.get.call(this, id, queryParameters);
+	var entity = JSON.parse(response.text);
+	return entity.floating_ip;
 };
 
 module.exports = FloatingIpsApi;
