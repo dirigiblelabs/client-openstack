5b4241fe53d53c63a8e15b2392ca64fd50f30f37
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-02-26 10:10:33 +0200

    Update Networks.js

--- openstack/network/Networks.js
+++ openstack/network/Networks.js
@@ -5,15 +5,15 @@ var method = Networks.prototype = Object.create(Api);
 method.constructor = Networks;
 
 function Networks(token) {
-    Api.constructor.apply(this, [token]);
+	Api.constructor.apply(this, [token]);
 }
 
 method.getMetadata = function() {
 	return {
-    	'host': ServiceRegistry.getNetworkService(),
-    	'version': 'v2.0',
-    	'kind': 'networks'
-    };
+		'host': ServiceRegistry.getNetworkService(),
+		'version': 'v2.0',
+		'kind': 'networks'
+	};
 };
 
 method.getApiPattern = function() {
@@ -21,15 +21,15 @@ method.getApiPattern = function() {
 };
 
 method.list = function(queryParameters) {
-    var response = Api.list.call(this, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.networks;
+	let response = Api.list.call(this, queryParameters);
+	let entity = JSON.parse(response.text);
+	return entity.networks;
 };
 
 method.get = function(id, queryParameters) {
-    var response = Api.get.call(this, id, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity;
+	let response = Api.get.call(this, id, queryParameters);
+	let entity = JSON.parse(response.text);
+	return entity;
 };
 
 module.exports = Networks;
