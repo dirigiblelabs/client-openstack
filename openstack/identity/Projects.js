584ca064d0ffc4ae1dffbc241415cb53fc91e25c
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-02-26 10:03:27 +0200

    Update Projects.js

--- openstack/identity/Projects.js
+++ openstack/identity/Projects.js
@@ -5,15 +5,15 @@ var method = Projects.prototype = Object.create(Api);
 method.constructor = Projects;
 
 function Projects(token) {
-    Api.constructor.apply(this, [token]);
+	Api.constructor.apply(this, [token]);
 }
 
 method.getMetadata = function() {
 	return {
-    	'host': ServiceRegistry.getIdentityService(),
-    	'version': 'v3',
-    	'kind': 'projects'
-    };
+		'host': ServiceRegistry.getIdentityService(),
+		'version': 'v3',
+		'kind': 'projects'
+	};
 };
 
 method.getApiPattern = function() {
@@ -21,15 +21,15 @@ method.getApiPattern = function() {
 };
 
 method.list = function(queryParameters) {
-    var response = Api.list.call(this, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.projects;
+	var response = Api.list.call(this, queryParameters);
+	var entity = JSON.parse(response.text);
+	return entity.projects;
 };
 
 method.get = function(id, queryParameters) {
-    var response = Api.get.call(this, id, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.project;
+	var response = Api.get.call(this, id, queryParameters);
+	var entity = JSON.parse(response.text);
+	return entity.project;
 };
 
 module.exports = Projects;
