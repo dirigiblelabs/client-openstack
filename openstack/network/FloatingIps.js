a0d6759edaf97b7f6ea958b29e95d0abcc84951c
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-02-26 10:09:52 +0200

    Update FloatingIps.js

--- openstack/network/FloatingIps.js
+++ openstack/network/FloatingIps.js
@@ -5,15 +5,15 @@ var method = FloatingIps.prototype = Object.create(Api);
 method.constructor = FloatingIps;
 
 function FloatingIps(token) {
-    Api.constructor.apply(this, [token]);
+	Api.constructor.apply(this, [token]);
 }
 
 method.getMetadata = function() {
 	return {
-    	'host': ServiceRegistry.getNetworkService(),
-    	'version': 'v2.0',
-    	'kind': 'floatingips'
-    };
+		'host': ServiceRegistry.getNetworkService(),
+		'version': 'v2.0',
+		'kind': 'floatingips'
+	};
 };
 
 method.getApiPattern = function() {
@@ -21,15 +21,15 @@ method.getApiPattern = function() {
 };
 
 method.list = function(queryParameters) {
-    var response = Api.list.call(this, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.floatingips;
+	let response = Api.list.call(this, queryParameters);
+	let entity = JSON.parse(response.text);
+	return entity.floatingips;
 };
 
 method.get = function(id, queryParameters) {
-    var response = Api.get.call(this, id, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.floatingip;
+	let response = Api.get.call(this, id, queryParameters);
+	let entity = JSON.parse(response.text);
+	return entity.floatingip;
 };
 
 module.exports = FloatingIps;
