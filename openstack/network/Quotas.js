b4d4b51bd6d9f7c99e3a6561c760684a7155e892
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-02-26 10:11:11 +0200

    Update Quotas.js

--- openstack/network/Quotas.js
+++ openstack/network/Quotas.js
@@ -5,15 +5,15 @@ var method = Quotas.prototype = Object.create(Api);
 method.constructor = Quotas;
 
 function Quotas(token) {
-    Api.constructor.apply(this, [token]);
+	Api.constructor.apply(this, [token]);
 }
 
 method.getMetadata = function() {
 	return {
-    	'host': ServiceRegistry.getNetworkService(),
-    	'version': 'v2.0',
-    	'kind': 'quotas'
-    };
+		'host': ServiceRegistry.getNetworkService(),
+		'version': 'v2.0',
+		'kind': 'quotas'
+	};
 };
 
 method.getApiPattern = function() {
@@ -21,15 +21,15 @@ method.getApiPattern = function() {
 };
 
 method.list = function(queryParameters) {
-    var response = Api.list.call(this, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.quotas;
+	let response = Api.list.call(this, queryParameters);
+	let entity = JSON.parse(response.text);
+	return entity.quotas;
 };
 
 method.get = function(id, queryParameters) {
-    var response = Api.get.call(this, id, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.quota;
+	let response = Api.get.call(this, id, queryParameters);
+	let entity = JSON.parse(response.text);
+	return entity.quota;
 };
 
 module.exports = Quotas;
