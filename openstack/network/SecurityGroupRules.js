9da519b55642df5bce7da44549b028ef24fa1057
Yordan Pavlov <ThuF@users.noreply.github.com>
2018-02-26 10:11:51 +0200

    Update SecurityGroupRules.js

--- openstack/network/SecurityGroupRules.js
+++ openstack/network/SecurityGroupRules.js
@@ -5,15 +5,15 @@ var method = SecurityGroupRules.prototype = Object.create(Api);
 method.constructor = SecurityGroupRules;
 
 function SecurityGroupRules(token) {
-    Api.constructor.apply(this, [token]);
+	Api.constructor.apply(this, [token]);
 }
 
 method.getMetadata = function() {
 	return {
-    	'host': ServiceRegistry.getNetworkService(),
-    	'version': 'v2.0',
-    	'kind': 'security-group-rules'
-    };
+		'host': ServiceRegistry.getNetworkService(),
+		'version': 'v2.0',
+		'kind': 'security-group-rules'
+	};
 };
 
 method.getApiPattern = function() {
@@ -21,15 +21,15 @@ method.getApiPattern = function() {
 };
 
 method.list = function(queryParameters) {
-    var response = Api.list.call(this, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.security_group_rules;
+	let response = Api.list.call(this, queryParameters);
+	let entity = JSON.parse(response.text);
+	return entity.security_group_rules;
 };
 
 method.get = function(id, queryParameters) {
-    var response = Api.get.call(this, id, queryParameters);
-    var entity = JSON.parse(response.text);
-    return entity.security_group_rule;
+	let response = Api.get.call(this, id, queryParameters);
+	let entity = JSON.parse(response.text);
+	return entity.security_group_rule;
 };
 
 module.exports = SecurityGroupRules;
